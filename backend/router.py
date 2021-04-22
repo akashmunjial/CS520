3af5dab1c51aed3b07ac9a47d3f72580a0fadec6
acg <aidancgreenslade@gmail.com>
2021-04-21 21:06:14 -0400

    Frontend input overhaul

--- backend/router.py
+++ backend/router.py
@@ -5,26 +5,25 @@ from backend.keys import api_key
 # This is a temporary implementation to get a full feedback loop
 def get_route(ori, dest, dist, tran, ele):
     
-    # translate geodata for osmnx
-    point_a = osmnx.geocoder.geocode(ori)
-    point_b = osmnx.geocoder.geocode(dest)
+    # translate geodata for osmnx '(lat, lng)' -> (lat, lng)
+    point_a = tuple(float(x) for x in ori[1:-1].split(', '))
+    point_b = tuple(float(x) for x in dest[1:-1].split(', '))
     n = max(point_a[0], point_b[0])
     s = min(point_a[0], point_b[0])
     e = max(point_a[1], point_b[1])
     w = min(point_a[1], point_b[1])
-    
-    # translate data for astar
-    graph = osmnx.graph.graph_from_bbox(n, s, e, w)
-    osmnx.elevation.add_node_elevations(graph, api_key=api_key)
-    start = osmnx.distance.get_nearest_node(graph, point_a)
-    # access elevation with graph.nodes[<node id>]['elevation']
-    end = osmnx.distance.get_nearest_node(graph, point_b)
-    ### get nearest node kinda sucks... simplify=False? ###
 
-    # run search algorithm
-    astar = AStar(graph)
+    # get nearest road nodes to origin and destination
+    road_graph = osmnx.graph.graph_from_bbox(n, s, e, w, simplify=False, network_type='drive')
+    start = osmnx.distance.get_nearest_node(road_graph, point_a)
+    end = osmnx.distance.get_nearest_node(road_graph, point_b)
+
+    # run search algorithm on unsimplified graph of type tran
+    astar_graph = osmnx.graph.graph_from_bbox(n, s, e, w, simplify=False, network_type=tran)
+    osmnx.elevation.add_node_elevations(astar_graph, api_key=api_key)
+    astar = AStar(astar_graph)
     route = astar.search(start, end)
 
     # convert nodes to coordinates
-    route_coords = [(graph.nodes[node]['y'], graph.nodes[node]['x']) for node in route]
+    route_coords = [(astar_graph.nodes[node]['y'], astar_graph.nodes[node]['x']) for node in route]
     return route_coords
--- frontend/map.js
+++ frontend/map.js
@@ -1,3 +1,5 @@
+// Create the map and provide mandatory OSM attribution
+
 var map = L.map('map', {
   center: [42.39, -72.52],
   zoom: 14
@@ -8,9 +10,11 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   minZoom: '0'
 }).addTo(map);
 
-// Dummy polyline
+// The send_request and update_map functions manage the communication to the backend
+// The names are self-explanatory
+// It also instantiates dummy polyline so it can be replaced later
+
 var polyline_route = L.polyline([0,0][0,0]);
-const submit = document.querySelector('#submit');
 
 function send_request() {
   console.log('Sending route request...');
@@ -32,12 +36,43 @@ function send_request() {
 function update_map(response) {
   let obj = JSON.parse(response);
   console.log('Painting route...');
-  // Currently pans to the starting point, for simplicity
-  map.panTo(new L.LatLng(obj.route[0][0], obj.route[0][1]));
-  polyline_route = L.polyline(obj.route).addTo(map);
+  // Pan to the center of the origin and destination
+  map.panTo(new L.LatLng((obj.route[obj.route.length-1][0] + obj.route[0][0]) / 2, 
+                         (obj.route[obj.route.length-1][1] + obj.route[0][1]) / 2));
+  polyline_route = L.polyline(obj.route, color='#ff0000');
+  polyline_route.setStyle({color:'red'});
+  polyline_route.addTo(map);
   console.log('Done.');
 }
+
+// Even listender to submit the route request
+
+const submit = document.querySelector('#submit');
 
 submit.addEventListener('click', function() {
   send_request();
 })
+
+// The following functions use mouse clicks to get coordinates
+// They also place a marker at the origin and destination
+// Note that the markers can be heavily customized
+
+var origin_marker = new L.marker();
+var origin_input = document.querySelector('#origin');
+
+var destination_marker = new L.marker();
+var destination_input = document.querySelector('#destination');
+
+map.on('click', function(e){
+  //console.log('Origin: (' + e.latlng.lat + ', ' + e.latlng.lng + ')');
+  origin_input.value = e.latlng.toString().substring(6);
+  origin_marker.remove();
+  origin_marker = L.marker(e.latlng).addTo(map);
+});
+
+map.on('contextmenu', function(e){
+  //console.log('Destination: (' + e.latlng.lat + ', ' + e.latlng.lng + ')');
+  destination_input.value = e.latlng.toString().substring(6);
+  destination_marker.remove();
+  destination_marker = L.marker(e.latlng).addTo(map);
+});
