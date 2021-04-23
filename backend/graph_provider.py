5fd7ed68e353411d310074efbdd061e8d6019f7d
dylanfinkbeiner <dfinkbei@ucsd.edu>
2021-04-23 13:25:49 -0700

    Fix small typo

--- backend/graph_provider.py
+++ backend/graph_provider.py
@@ -2,13 +2,13 @@ from abc import ABC, abstractmethod
 
 class GraphProvider(ABC):
     @abstractmethod
-    def get_neighbors(node):
+    def get_neighbors(self, node):
         pass
 
     @abstractmethod
     def get_edge_distance(self, node1, node2):
         pass
 
-    @abtractmethod
+    @abstractmethod
     def get_coords(self, node):
         pass
