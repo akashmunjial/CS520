05fd69ddabc2a76b220131f3be6ccfb71b17c6cc
dylanfinkbeiner <dfinkbei@ucsd.edu>
2021-04-23 10:44:33 -0700

    Add naive utilization of elevation data

--- backend/dijkstra.py
+++ backend/dijkstra.py
@@ -8,22 +8,29 @@ from backend.keys import api_key
 Essentially follows the implementation here: https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm#Using_a_priority_queue
 '''
 class Dijkstra:
-    def __init__(self, graph):
+    def __init__(self, graph, use_elevation=False, max_path_len=None):
         self.graph = graph
+        if use_elevation and max_path_len is None:
+            assert max_path_len is not None, "If we are going to use elevation data, we need a maximum path length we cannot exceed"
+        self.use_elevation = use_elevation
 
     def distance(self, node1, node2):
         return self.graph.get_edge_data(node1, node2)[0]['length'] # TODO: what if len(array) != 1
+
+    def elevation(self, node):
+        return self.graph.nodes[node]['elevation']
 
     def search(self, start, end):
         prev = {}
         dist = defaultdict(lambda: math.inf)
         visited = set()
         priority_queue = []
+        subtract = {}
 
         prev[start] = None
         dist[start] = 0
-        heapq.heappush(priority_queue, (dist[start], start))
-
+        heapq.heappush(priority_queue, (dist[start], start, 0.))
+        subtract[start] = 0.
         while len(priority_queue) > 0:
             curr_dist, curr_node = heapq.heappop(priority_queue)
 
@@ -34,29 +41,41 @@ class Dijkstra:
             if curr_node == end:
                 path = [end]
                 predecessor = prev[end]
+                subtract_total = subtract[end] # Initialize
                 while predecessor is not None:
                     path.append(predecessor)
+                    subtract_total = subtract_total + subtract[predecessor]
                     predecessor = prev[predecessor]
                 path.reverse() # Make list begin with 'start' node
-                print('Path Found: ', path)
-                return path
+                path_len = dist[end] - subtract_total
+                return path, path_len
 
             neighbors = self.graph.neighbors(curr_node)
             for n in neighbors:
                 if n in visited:
                     continue
-                alt_path_dist = curr_dist + self.distance(curr_node, n)
-                if alt_path_dist < dist[n]:
-                    dist[n] = alt_path_dist
+
+                if self.use_elevation:
+                    elev_diff = self.elevation(n) - self.elevation(curr_node)
+                    heuristic_weight = max([0, elev_diff])
+                    alt_path_weight = curr_dist + self.distance(curr_node, n) + heuristic_weight
+                    subtract_this = heuristic_weight
+                else:
+                    alt_path_weight = curr_dist + self.distance(curr_node, n)
+                    subtract_this = 0.
+
+                if alt_path_weight < dist[n]:
+                    dist[n] = alt_path_weight
+                    subtract[n] = subtract_this
                     prev[n] = curr_node
-                    heapq.heappush(priority_queue, (alt_path_dist, n))
+                    heapq.heappush(priority_queue, (alt_path_weight, n))
 
         """
-        Should we only allow a node to appear once in the priority queue? This
-        is memory-efficient, but requires us to do a linear search every time we
+        Should we only allow a node to appear once in the priority queue? That would
+        be memory-efficient, but requires us to do a linear search every time we
         want to update a node's priority queue value.
         """
 
         print("No path found")
-        return []
+        return [], math.inf
 
