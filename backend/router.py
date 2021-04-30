89ad16c2edbb0034ed0520d4dc0f3b54b820004f
Akash Munjial <amunjial@umass.edu>
2021-04-30 14:44:59 -0400

    Updated router.py

--- backend/router.py
+++ backend/router.py
@@ -18,72 +18,26 @@ def get_route(ori, dest, dist, ele, grph):
         print('User did not select origin and/or destination')
         return []
 
-
-
-#     #astar = AStar(graph_provider)
-#     dijkstra = Dijkstra(graph_provider)
-#     max_search = MidpointMiracle(graph_provider)
-
-#     #route = astar.search(start, end)
-#     shortest_res = dijkstra.search(start, end)
-#     shortest_path = shortest_res['path']
-#     shortest_path_len = shortest_res['path_len']
-#     shortest_path_gain = shortest_res['ele_gain']
-
-#     if ele != 'shortest':
-#         best_res = shortest_res # Default
-#         if ele == 'maximal':
-#             result = max_search.search(
-#                     start, end,
-#                     max_path_len=(dist / 100)*shortest_path_len)
-#             best_ele_gain = shortest_path_gain
-#             if result['ele_gain'] > best_ele_gain:
-#                 best_ele_gain = result['ele_gain']
-#                 best_res = result
-#         else:
-#             # Forward
-#             forward_res = dijkstra.search(
-#                     start, end, use_elevation=True,
-#                     max_path_len=(dist / 100)*shortest_path_len,
-#                     backward=False)
-#             # Backward
-#             backward_res = dijkstra.search(
-#                     end, start, use_elevation=True,
-#                     max_path_len=(dist / 100)*shortest_path_len,
-#                     backward=True)
-#             best_ele_gain = shortest_path_gain
-#             for result in [forward_res, backward_res]:
-#                 if result['ele_gain'] < best_ele_gain:
-#                     best_ele_gain = result['ele_gain']
-#                     best_res = result
-
-#         route = best_res['path']
-#     else:
-#         route = shortest_path
-
-#     route_coords = [(node['y'], node['x']) for node in map(graph_provider.get_coords, route)]
-
-#     # convert nodes to coordinates
-#     return route_coords
     # Choose graph type
     if(grph == 'bounded'):
         graph_provider = BoundedGraphProvider(start_coords, end_coords)
     else:
         graph_provider = LoadingGraphProvider(start_coords, end_coords)
-    print(ele)
-    # Choose between Dijkstra and AStar based on form input
-    # if(ele == 'shortest'):
-    #     search_algo = Dijkstra(graph_provider)
-    # else:
-    #     search_algo = AStar(graph_provider, ele, int(dist) * 1000000)
-    # route = search_algo.search(graph_provider.start, graph_provider.end)
-    shortest = Dijkstra(graph_provider)
-    res = shortest.search(graph_provider.start, graph_provider.end)
-    search = AStar(graph_provider, ele, int(dist)*res['path_len']/100)
-    result = search.search(graph_provider.start, graph_provider.end)
-    route = result['path']
-    stats = [1000, 100, 1500, 500]
-    
-    # convert nodes to coordinates and return
+    shortest = AStar(graph_provider, 'shortest')
+    shortest_res = shortest.search(graph_provider.start, graph_provider.end)
+
+    limit = shortest_res['path_len']*int(dist)/100
+    if ele == 'minimal':
+        minimal = AStar(graph_provider, 'minimal', limit)
+        res = minimal.search(graph_provider.start, graph_provider.end)
+    elif ele == 'maximal':
+        maximal = MidpointMiracle(graph_provider)
+        res = maximal.search(graph_provider.start, graph_provider.end, max_path_len = limit)
+    else:
+        res = shortest_res
+
+    stats = [round(float(shortest_res['path_len'])), round(float(shortest_res['ele_gain'])), round(float(res['path_len'])), round(float(res['ele_gain']))]
+    route = res['path']
+
     route_coords = [(node['y'], node['x']) for node in map(graph_provider.get_coords, route)]
     return route_coords, stats
