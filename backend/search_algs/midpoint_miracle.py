c548bb7eeec74c69f1e9b78d8fa152cd8f7653a3
dylanfinkbeiner <dfinkbei@ucsd.edu>
2021-05-02 23:13:11 -0700

    Fix bugs in computing contributions and pruning

--- backend/search_algs/midpoint_miracle.py
+++ backend/search_algs/midpoint_miracle.py
@@ -56,15 +56,17 @@ class MidpointMiracle:
 
         def sort_func(node):
             node_ele = self.__elevation(node)
-            if node_ele < ele_start:
-                # We will gain at least this much while traveling
-                # from node to end
-                value = max([0., ele_end - node_ele])
-            else:
-                # We will gain at least this much by traveling from
-                # start to node
-                value = node_ele - ele_start
-            return value
+            potential_gain_prefix = max([0., node_ele - ele_start])
+            potential_gain_suffix = max([0., ele_end - node_ele])
+            #if node_ele < ele_start:
+            #    # We will gain at least this much while traveling
+            #    # from node to end
+            #    value = max([0., ele_end - node_ele])
+            #else:
+            #    # We will gain at least this much by traveling from
+            #    # start to node
+            #    value = node_ele - ele_start
+            return max([potential_gain_prefix, potential_gain_suffix])
 
         return sort_func
 
@@ -111,23 +113,30 @@ class MidpointMiracle:
             dist = ss_result['dist']
 
             path = [end]
+            # For forward direction, this is the diff between the second-to-last-node and 'end'
+            # but for backward direction, this is the diff between 'end', which is really the start, and the second node on the path
+            #end_contribution = max([0., -ele_diff[end]]) if backward else max([0., ele_diff[end]])
             end_contribution = 0. if backward else max([0., ele_diff[end]])
             cum_ele_diff = end_contribution
-            predecessor = prev[end]
+            successor = end
+            curr_node = prev[end]
             contributions = {} # To cumulative elevation gain
             contributions[end] = end_contribution
-            while predecessor is not None:
-                path.append(predecessor)
+            while curr_node is not None:
+                path.append(curr_node)
                 if backward:
-                    contribution = max([0., -ele_diff[predecessor]])
+                    # contribution should be ele_diff of the successor
+                    contribution = max([0., -ele_diff[successor]])
                 else:
-                    contribution = max([0., ele_diff[predecessor]])
+                    contribution = max([0., ele_diff[curr_node]])
                 cum_ele_diff += contribution
-                contributions[predecessor] = contribution
-                predecessor = prev[predecessor]
+                contributions[curr_node] = contribution
+                curr_node = prev[curr_node]
+                successor = curr_node
 
             if not backward:
                 path.reverse() # Make list begin with 'start' node
+
             path_len = dist[end]
 
             result = {
@@ -147,14 +156,17 @@ class MidpointMiracle:
         while(len(sorted_candidates) > 0 and len(selected_midpoints) < keep_n):
             curr = sorted_candidates.pop(0)
             selected_midpoints.append(curr)
-            neighbors = set(self.graph_provider.get_neighbors(curr))
-            old_neighbors = neighbors
-            for _ in range(prune_depth - 1):
-                new_neighbors = [n for node in old_neighbors for n in self.graph_provider.get_neighbors(node)]
-                new_neighbors = set(new_neighbors)
-                neighbors = neighbors.union(new_neighbors)
-                old_neighbors = new_neighbors
-            sorted_candidates = [node for node in sorted_candidates if node not in neighbors]
+
+            # Pruning
+            if prune_depth > 0:
+                neighbors = set(self.graph_provider.get_neighbors(curr))
+                old_neighbors = neighbors
+                for _ in range(prune_depth - 1):
+                    new_neighbors = [n for node in old_neighbors for n in self.graph_provider.get_neighbors(node)]
+                    new_neighbors = set(new_neighbors)
+                    neighbors = neighbors.union(new_neighbors)
+                    old_neighbors = new_neighbors
+                sorted_candidates = [node for node in sorted_candidates if node not in neighbors]
         return selected_midpoints
 
     def __obtain_full_result(self, midpoint):
@@ -180,7 +192,12 @@ class MidpointMiracle:
         selected_midpoints = self.__select_and_prune(sorted_candidates, keep_n, prune_depth)
 
         results = [self.__obtain_full_result(midpoint) for midpoint in selected_midpoints]
-        best_result = max(results, key=lambda r: r.ele_gain)
 
-        return best_result
+        best_res = SearchResult(path=[], path_len=inf, ele_gain=-inf)
+        for r in results:
+            if r.ele_gain > best_res.ele_gain:
+                best_res = r
+        #best_result = max(results, key=lambda r: r.ele_gain)
+
+        return best_res
 
