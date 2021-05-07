ee7820bc36d0fcbb4688d27b2e5b1b88d5383ff1
alexhulbert <alex@alexhulbert.com>
2021-05-06 21:23:58 -0400

    Adjust frontend code to be more idiomatic

--- frontend/map.js
+++ frontend/map.js
@@ -1,148 +1,123 @@
 // Create the map (defaulted to Amherst) and provide mandatory OSM attribution
 
-var map = L.map('map', {
+const map = L.map('map', {
   center: [42.39, -72.52],
   zoom: 14
-});
+})
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  attribution: '&copy <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
   minZoom: '0'
-}).addTo(map);
+}).addTo(map)
 
 // functions to pan the map to a specified location
 
-function send_search_request() {
-  console.log('Sending search request...');
-  let request = new XMLHttpRequest();
-  // Set the callback function once the coords are returned
-  request.onreadystatechange = function() {
-    if(request.readyState === 4) {
-      console.log('Coords recieved.');
-      update_map_search(request.response);
-    }
-  }
-  // Open and send the request
-  request.open('POST', '/search');
-  request.send(new FormData(document.querySelector('#search-form')));
+async function sendSearchRequest() {
+  const resp = await fetch('/search', {
+    method: 'POST',
+    body: new FormData(document.getElementById('search-form'))
+  })
+  updateMapSearch(await resp.json())
 }
 
-const search_submit = document.querySelector('#search-submit');
+const searchSubmit = document.getElementById('search-submit')
 
-function update_map_search(response) {
-  let obj = JSON.parse(response);
-  if(obj.coords.length === 0) {
-    alert('Place not found');
-  } else {
+function updateMapSearch({ coords }) {
+  if (coords.length) {
     // Pan to the requested place
-    map.panTo(new L.LatLng(obj.coords[0], obj.coords[1]));
-  }
-}
-
-// Event listener to submit the route request
-
-search_submit.addEventListener('click', function() {
-  send_search_request();
-})
-
-// The send_route_request and update_map_route functions manage the communication to the backend
-// The names are self-explanatory
-// It also instantiates dummy polyline so it can be replaced later
-
-var new_route = L.polyline([0,0][0,0]);
-var short_route = L.polyline([0,0][0,0]);
-
-function send_route_request() {
-  console.log('Sending route request...');
-  // Immediately erase existing route
-  new_route.remove();
-  short_route.remove();
-  let request = new XMLHttpRequest();
-  // Set the callback function once the route is loaded
-  request.onreadystatechange = function() {
-    if(request.readyState === 4) {
-      console.log('Route recieved.');
-      update_map_route(request.response);
-    }
-  }
-  // Open and send the request
-  request.open('POST', '/api');
-  request.send(new FormData(document.querySelector('#form')));
-}
-
-const submit = document.querySelector('#submit');
-const throbber = document.querySelector('#throbber');
-const stats = document.querySelector('#stats');
-
-function update_map_route(response) {
-  let obj = JSON.parse(response);
-  if (obj.error === 'timeout') {
-    alert('Timed out while looking for path');
-    console.log('Timed out');
-  } else if (obj.error === 'badcoords') {
-    alert('Please select both an origin and a destination');
-    console.log('Missing coords');
-  } else if(obj.route.length === 0) {
-    alert('No path found');
-    console.log('No path found');
+    map.panTo(new L.LatLng(coords[0], coords[1]))
   } else {
-    console.log('Painting route...');
-    // Pan to the center of the origin and destination
-    map.panTo(new L.LatLng((obj.route[obj.route.length-1][0] + obj.route[0][0]) / 2, 
-                          (obj.route[obj.route.length-1][1] + obj.route[0][1]) / 2));
-    short_route = L.polyline(obj.short_route);
-    short_route.setStyle({color:'blue'});
-    short_route.addTo(map);
-    new_route = L.polyline(obj.route);
-    new_route.setStyle({color:'red'});
-    new_route.addTo(map);
-    console.log('Done.');
-    // Print stats
-    if(obj.stats[2]==-1 && obj.stats[3]==-1) {
-      pretty_stats =    '        Shortest path length: ' + obj.stats[0] + 'm\r\n'
-                      + 'Shortest path elevation gain: ' + obj.stats[1] + 'm';
-    } else {
-      pretty_stats =    '        Shortest path length: ' + obj.stats[0] + 'm\r\n'
-                      + 'Shortest path elevation gain: ' + obj.stats[1] + 'm\r\n'
-                      + '             New path length: ' + obj.stats[2] + 'm\r\n'
-                      + '     New path elevation gain: ' + obj.stats[3] + 'm';
-    }
-    stats.textContent = pretty_stats;
-    stats.style.visibility = 'visible';
+    alert('Place not found')
   }
-  submit.disabled = false;
-  throbber.style.visibility = 'hidden';
 }
 
 // Event listener to submit the route request
 
-submit.addEventListener('click', function() {
-  send_route_request();
-  submit.disabled = true;
-  stats.style.visibility = 'hidden';
-  throbber.style.visibility = 'visible';
+searchSubmit.addEventListener('click', sendSearchRequest)
+
+// The sendRouteRequest and updateMapRoute functions manage the communication to the backend
+
+let newRouteLine, shortRouteLine
+
+async function sendRouteRequest() {
+  const resp = await fetch('/api', {
+    method: 'POST',
+    body: new FormData(document.getElementById('form'))
+  })
+  updateMapRoute(await resp.json())
+}
+
+const submit = document.getElementById('submit')
+const throbber = document.getElementById('throbber')
+const statsBox = document.getElementById('stats')
+
+function updateMapRoute({ error, stats, route, short_route: shortRoute }) {
+  if (error === 'timeout') {
+    alert('Timed out while looking for path')
+  } else if (error === 'badcoords') {
+    alert('Please select both an origin and a destination')
+  } else if (!route.length) {
+    alert('No path found')
+  } else {
+    // Pan to the center of the origin and destination
+    map.panTo(new L.LatLng(
+      (route[route.length-1][0] + route[0][0]) / 2, 
+      (route[route.length-1][1] + route[0][1]) / 2
+    ))
+    shortRouteLine = L.polyline(shortRoute)
+    shortRouteLine.setStyle({color:'blue'})
+    shortRouteLine.addTo(map)
+    newRouteLine = L.polyline(route)
+    newRouteLine.setStyle({color:'red'})
+    newRouteLine.addTo(map)
+    // Print stats
+    const prettyStats = [
+      `Shortest path length: ${stats[0]}m`,
+      `Shortest path elevation gain: ${stats[1]}m`
+    ]
+    if (stats[2] !== -1 || stats[3] !== -1) {
+      prettyStats.push(
+        `New path length: ${stats[2]}m`,
+        `New path elevation gain: ${stats[3]}m`
+      )
+    }
+    statsBox.textContent = prettyStats.join('\n')
+    statsBox.style.visibility = 'visible'
+  }
+  submit.disabled = false
+  throbber.style.visibility = 'hidden'
+}
+
+// Event listener to submit the route request
+
+submit.addEventListener('click', () => {
+  // Immediately erase existing route and show spinner
+  newRouteLine?.remove()
+  shortRouteLine?.remove()
+  submit.disabled = true
+  statsBox.style.visibility = 'hidden'
+  throbber.style.visibility = 'visible'
+  sendRouteRequest()
 })
 
 // The following functions use mouse clicks to get coordinates
 // They also place a marker at the origin and destination
 // Note that the markers can be heavily customized
 
-var origin_marker = new L.marker();
-var origin_input = document.querySelector('#origin');
+let originMarker = new L.marker()
+const originInput = document.getElementById('origin')
 
-var destination_marker = new L.marker();
-var destination_input = document.querySelector('#destination');
+let destinationMarker = new L.marker()
+const destinationInput = document.getElementById('destination')
 
-map.on('click', function(e){
-  //console.log('Origin: (' + e.latlng.lat + ', ' + e.latlng.lng + ')');
-  origin_input.value = e.latlng.toString().substring(6);
-  origin_marker.remove();
-  origin_marker = L.marker(e.latlng).addTo(map);
-});
+map.on('click', e => {
+  originInput.value = e.latlng.toString().slice(6)
+  originMarker.remove()
+  originMarker = L.marker(e.latlng).addTo(map)
+})
 
-map.on('contextmenu', function(e){
-  //console.log('Destination: (' + e.latlng.lat + ', ' + e.latlng.lng + ')');
-  destination_input.value = e.latlng.toString().substring(6);
-  destination_marker.remove();
-  destination_marker = L.marker(e.latlng).addTo(map);
-});
+map.on('contextmenu', e => {
+  destinationInput.value = e.latlng.toString().slice(6)
+  destinationMarker.remove()
+  destinationMarker = L.marker(e.latlng).addTo(map)
+})
