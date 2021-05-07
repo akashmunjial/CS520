0b3b305cbcb601937ff1ae18509d4a29e0bff2bf
Akash Munjial <amunjial@umass.edu>
2021-05-07 16:34:47 -0400

    Added comments

--- backend/search_algorithms/search_algorithm.py
+++ backend/search_algorithms/search_algorithm.py
@@ -2,10 +2,20 @@ from abc import ABC, abstractmethod
 
 
 class SearchAlgorithm(ABC):
+    """Abstract class representing the necessary requirements for a class performing a search algorithm.
+    """
+
     @abstractmethod
     def __init__(self, graph_provider):
         pass
 
+    """The function to execute the search from some start to some end node
+
+    Args:
+        start: the start of the search, the source
+        end: the termination point of the search, the target
+        **kwargs: Additional parameters that can be set such as the max_path_len etc.
+    """
     @abstractmethod
     def search(self, start, end, **kwargs):
         pass
--- backend/search_algorithms/search_result.py
+++ backend/search_algorithms/search_result.py
@@ -1,4 +1,11 @@
 class SearchResult(object):
+    """Class representing a return object for a search query.
+
+    Attributes:
+        path: an array representing the path from a start node to the end node, empty if there is no path
+        path_len: The length of the path represented by path, 0 if path is empty
+        ele_gain: The cumulative elevation gain through the path, 0 if path is empty
+    """
     def __init__(self, path=[], path_len=0, ele_gain=0):
         self.path = path
         self.path_len = path_len
